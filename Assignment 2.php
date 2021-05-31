b035bf20b345f2cc30b2d5255af695b9e09364c7
Rushikeshkeskar <81505552+Rushikeshkeskar@users.noreply.github.com>
2021-05-31 08:07:01 +0530

    Add files via upload

---
+++ Assignment 2.php
@@ -0,0 +1,18 @@
+Student_name{
+color:yellow;
+font-familt: Bodoni MT;
+font-style: italic;
+font-size: 14 pts;
+}
+Class_name{
+color:Orange;
+font-familt: Helvetica;
+font-style: Normal;
+font-size: 10 pts;
+}
+percentage{
+color:pink;
+font-familt: Impact;
+font-style: Oblique;
+font-size: 18 pts;
+}
